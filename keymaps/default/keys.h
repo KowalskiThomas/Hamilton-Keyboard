5d17b40c205321e1ba6c639e5683158225ae0e18
Thomas Kowalski <thomaskowalski@outlook.com>
2017-01-03 19:30:57 +0100

    Added greek and mathematical symbols to keys.h and wrote the keymap code. Still can't use X, which is weird, so I'm using UC (for now.)

--- keymaps/default/keys.h
+++ keymaps/default/keys.h
@@ -1,77 +1,188 @@
-// I don't get any pride out of this.
+// Normal keys
+    // I don't get any pride out of this.
+    #define ESC KC_ESC
+    #define F1 KC_F1
+    #define F1 KC_F1
+    #define F3 KC_F3
+    #define F4 KC_F4
+    #define F5 KC_F5
+    #define F6 KC_F6
+    #define F7 KC_F7
+    #define F8 KC_F8
+    #define F9 KC_F9
+    #define F10 KC_F10
+    #define F11 KC_F11
+    #define F12 KC_F12
+    #define FN2 KC_FN2
+    #define PSCR KC_PSCR
+    #define HOME KC_HOME
+    #define GRV KC_GRV
+    #define MINS KC_MINS
+    #define EQL KC_EQL
+    #define BSPC KC_BSPC
+    #define END KC_END
+    #define TAB KC_TAB
+    #define Q KC_Q
+    #define W KC_W
+    #define E KC_E
+    #define R KC_R
+    #define T KC_T
+    #define Y KC_Y
+    #define U KC_U
+    #define I KC_I
+    #define O KC_O
+    #define P KC_P
+    #define LBRC KC_LBRC
+    #define RBRC KC_RBRC
+    #define ENT KC_ENT
+    #define PGUP KC_PGUP
+    #define CAPS KC_CAPS
+    #define A KC_A
+    #define S KC_S
+    #define D KC_D
+    #define F KC_F
+    #define G KC_G
+    #define H KC_H
+    #define J KC_J
+    #define K KC_K
+    #define L KC_L
+    #define SCLN KC_SCLN
+    #define QUOT KC_QUOT
+    #define NUHS KC_NUHS
+    #define PGDN KC_PGDN
+    #define LSFT KC_LSFT
+    #define Z KC_Z
+    #define X KC_X
+    #define C KC_C
+    #define V KC_V
+    #define B KC_B
+    #define N KC_N
+    #define M KC_M
+    #define COMM KC_COMM
+    #define DOT KC_DOT
+    #define SLSH KC_SLSH
+    #define RCTL KC_RCTL
+    #define RSFT KC_RSFT
+    #define UP KC_UP
+    #define DEL KC_DEL
+    
+    #define FN0 KC_FN0
+    #define FN1 KC_FN1
+    #define FN2 KC_FN2
+    #define FN3 KC_FN3
+    
+    #define LCTL KC_LCTL
+    #define LGUI KC_LGUI
+    #define LALT KC_LALT
+    #define SPC KC_SPC
 
-#define ESC KC_ESC
-#define F1 KC_F1
-#define F1 KC_F1
-#define F3 KC_F3
-#define F4 KC_F4
-#define F5 KC_F5
-#define F6 KC_F6
-#define F7 KC_F7
-#define F8 KC_F8
-#define F9 KC_F9
-#define F10 KC_F10
-#define F11 KC_F11
-#define F12 KC_F12
-#define FN2 KC_FN2
-#define PSCR KC_PSCR
-#define HOME KC_HOME
-#define GRV KC_GRV
-#define MINS KC_MINS
-#define EQL KC_EQL
-#define BSPC KC_BSPC
-#define END KC_END
-#define TAB KC_TAB
-#define Q KC_Q
-#define W KC_W
-#define E KC_E
-#define R KC_R
-#define T KC_T
-#define Y KC_Y
-#define U KC_U
-#define I KC_I
-#define O KC_O
-#define P KC_P
-#define LBRC KC_LBRC
-#define RBRC KC_RBRC
-#define ENT KC_ENT
-#define PGUP KC_PGUP
-#define CAPS KC_CAPS
-#define A KC_A
-#define S KC_S
-#define D KC_D
-#define F KC_F
-#define G KC_G
-#define H KC_H
-#define J KC_J
-#define K KC_K
-#define L KC_L
-#define SCLN KC_SCLN
-#define QUOT KC_QUOT
-#define NUHS KC_NUHS
-#define PGDN KC_PGDN
-#define LSFT KC_LSFT
-#define Z KC_Z
-#define X KC_X
-#define C KC_C
-#define V KC_V
-#define B KC_B
-#define N KC_N
-#define M KC_M
-#define COMM KC_COMM
-#define DOT KC_DOT
-#define SLSH KC_SLSH
-#define RCTL KC_RCTL
-#define RSFT KC_RSFT
-#define UP KC_UP
-#define DEL KC_DEL
-#define LCTL KC_LCTL
-#define LGUI KC_LGUI
-#define LALT KC_LALT
-#define SPC KC_SPC
-#define FN0 KC_FN0
-#define FN1 KC_FN1
-#define LEFT KC_LEFT
-#define RIGHT KC_RIGHT
-#define DOWN KC_DOWN
-#define RALT KC_RALT
+
+    #define LEFT KC_LEFT
+    #define RIGHT KC_RIGHT
+    #define DOWN KC_DOWN
+    #define RALT KC_RALT
+
+// Greek letters Unicode values
+    //Upper Case (Capital_)
+    #define C_SIGMA   0x03A3
+    #define C_THETA   0x0398
+    #define C_LAMBDA  0x039B
+    #define C_DELTA   0x0394
+    #define C_OMEGA   0x03A9
+    #define C_PHI     0x03A6
+    #define C_PSI     0x03A8
+    #define C_CHI     0x03A7
+    #define C_XI      0x039E
+    #define C_GAMMA   0x0393
+    #define C_PI      0x03A0
+
+    //Lower Case
+    #define ALPHA   0x03B1
+    #define BETA    0x03B2
+    #define GAMMA   0x03B3
+    #define XI      0x03BE
+    #define ZETA    0x03B6
+    #define ETA     0x03B7
+    #define PI      0x03C0
+    #define PSI     0x03C9
+    #define PHI     0x03C6
+    #define CHI     0x03C7
+    #define OMEGA   0x03C8
+    #define DELTA   0x03B4
+    #define TAU     0x03C4
+    #define SIGMA   0x03C3
+    #define LAMBDA  0x03BB
+    #define KC_MU   0x03BC //MU is already used.
+    #define RHO     0x03C1
+    #define EPSILON 0x03B5
+    #define THETA   0x03B8
+    #define NU      0x03BD
+
+// Mathematical symbols
+    //Blackboard
+    #define BBR 0x211D
+    #define BBU 0x1D54C
+    #define BBQ 0x211A
+    #define BBZ 0x2124
+    #define BBN 0x2115
+    #define BBC 0x2102
+    #define BBK 0x1D542
+
+    //Square root
+    #define SQRT 0x221A
+    //Logical not
+    #define LNOT 0x00AC
+    //Logical or
+    #define LOR 0x2228
+    //Logical and
+    #define LAND 0x2227
+    //in set (0x0220B is in the other way)
+    #define _IN 0x2208 
+    //not in set
+    #define _NIN 0x2209
+    //for all
+    #define ALL 0x2200
+    //proportionnal
+    #define PROP 0x221D
+    //implies
+    #define IF 0x21D2
+    //if and only if 
+    #define IFF 0x21D4 //(0x27FA is larger, can be used)
+    //equivalent (classes)
+    #define EQUV 0x2261
+    //plus or minus
+    #define PSMN 0x00B1
+    //partial derivative
+    #define PART 0x2202
+    //empty set
+    #define EMPT 0x2205
+    //times
+    #define TIMS 0x00D7
+    //more or less equal (≃ is 0x2243)
+    #define APPX 0x2248
+    //nabla
+    #define NABL 0x2207
+    //<<
+    #define INFR 0x27EA
+    //>>
+    #define SUPR 0x27EB
+    //inter (0x22C2 is bigger)
+    #define INTR 0x2229
+    //union (0x22C3 is bigger)
+    #define UNIN 0x222A
+    //infinity
+    #define INFY 0x221E
+    //sum
+    #define INTG 0x222B
+    //divide
+    #define DIVD 0x00F7
+    //different
+    #define DIFF 0x2260
+    //exists
+    #define EXIS 0x2203
+    
+    //Arrows
+    #define UPAR 0x2191
+    #define DNAR 0x2193
+    #define LEAR 0x2190
+    #define RIAR 0x2192
